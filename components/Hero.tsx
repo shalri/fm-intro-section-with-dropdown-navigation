2c40908cf24be31612f45fb35d8fb05a23519b45
shalri <fsaonlinework777@gmail.com>
2024-09-10 11:56:29 +0800

    Hero desktop layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,8 @@ import Runner from "./Runner";
 
 export default function SandBox() {
   return (
-    <main className="flex flex-grow flex-col pb-16 sm:mx-auto sm:mt-10 sm:grid sm:max-w-[1185px] sm:grid-cols-2">
-      <picture className="relative min-h-[288px] w-full sm:col-start-2 sm:flex sm:min-h-[638px] sm:w-[82%] sm:place-self-end">
+    <main className="flex flex-grow flex-col pb-16 sm:mx-auto sm:mt-10 sm:grid sm:max-w-[1185px] sm:grid-cols-2 sm:items-start">
+      <picture className="relative min-h-[288px] w-full sm:col-start-2 sm:ml-auto sm:flex sm:min-h-[638px] sm:w-[82%] sm:place-self-start">
         <source
           srcSet="/images/image-hero-mobile.png"
           media="(max-width: 640px)"
@@ -21,18 +21,18 @@ export default function SandBox() {
           className="object-contain"
         />
       </picture>
-      <section className="mt-10 flex flex-col items-center justify-center px-[18px] text-center sm:row-start-1">
-        <h1 className="text-nowrap text-[35px] font-bold text-is-almost-black">
+      <section className="sm:itmes-start mt-10 flex flex-col items-center justify-center overflow-hidden px-[18px] text-center sm:row-start-1 sm:mt-0 sm:items-start sm:px-[2.42rem] sm:pt-[102px] sm:text-left">
+        <h1 className="w-full text-nowrap text-[35px] font-bold text-is-almost-black sm:text-wrap sm:text-[4.85rem] sm:leading-[1.05]">
           {heroCopy.header}
         </h1>
-        <p className="mt-2 leading-[1.65] text-is-medium-gray">
+        <p className="mt-2 max-w-[450px] leading-[1.65] text-is-medium-gray sm:mt-[46px] sm:text-[18px] sm:leading-[1.585]">
           {heroCopy.body}
         </p>
-        <button className="mt-6 rounded-2xl bg-is-almost-black px-6 py-3 text-is-almost-white">
+        <button className="mt-6 rounded-2xl bg-is-almost-black px-6 py-3 text-is-almost-white sm:mt-[50px] sm:px-[30px] sm:py-[15px] sm:text-[18px]">
           Learn more
         </button>
+        <Runner />
       </section>
-      <Runner />
     </main>
   );
 }
