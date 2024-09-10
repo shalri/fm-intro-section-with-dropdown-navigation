ffa2cdda8c6523797a83ceb74bde1f727262ff1f
shalri <fsaonlinework777@gmail.com>
2024-09-10 11:56:54 +0800

    Runner desktop layout

--- components/Runner.tsx
+++ components/Runner.tsx
@@ -8,22 +8,25 @@ import { useState } from "react";
 export default function Runner() {
   const [isPlaying, setIsPlaying] = useState(true);
   return (
-    <footer className="max-w-screen mt-12 overflow-hidden">
-      <ul className="flex items-center justify-between gap-x-2 px-[18px]">
+    <footer className="mt-12 w-full overflow-hidden sm:mt-[110px]">
+      <ul className="flex items-center gap-x-8">
         <Ticker
           duration={20}
-          isPlaying={isPlaying}
+          isPlaying={!isPlaying}
           onMouseEnter={() => setIsPlaying(false)}
           onMouseLeave={() => setIsPlaying(true)}
         >
           {runnerLogos.map((logo) => (
             <li
               className={cn(
-                "relative mx-2 h-[24px] w-[58px]",
-                logo.company === "Databiz" && "h-[25px] w-[80px]",
-                logo.company === "Audiophile" && "h-[25px] w-[54px]",
-                logo.company === "Meet" && "h-[25px] w-[60px]",
-                logo.company == "Maker" && "[h-25px] w-[58px]",
+                "relative ml-8 h-[25px] sm:h-[28px]",
+                logo.company === "Databiz" && "w-[80px] sm:w-[112px]",
+                logo.company === "Audiophile" &&
+                  "w-[54px] sm:h-[35px] sm:w-[100px]",
+                logo.company === "Meet" &&
+                  "h-[25px] w-[60px] sm:h-[28px] sm:w-[86px]",
+                logo.company == "Maker" &&
+                  "h-[25px] w-[58px] sm:h-[28px] sm:w-[86px]",
               )}
               key={logo.company}
             >
