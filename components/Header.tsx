c152b15f7c7fa67252277c2e033921e73392ed4e
shalri <fsaonlinework777@gmail.com>
2024-09-25 22:07:59 +0800

    Fix nav flicker during initial page load in mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Navigation from "./Navigation";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   return (
     <header className="mx-auto flex w-full max-w-[1394px] items-center px-[18px] py-5 sm:items-center sm:py-[20px]">
       <Link href="/" aria-label="Homepage">
@@ -15,7 +19,7 @@ export default function Header() {
           />
         </div>
       </Link>
-      <Navigation />
+      {mounted && <Navigation />}
     </header>
   );
 }
